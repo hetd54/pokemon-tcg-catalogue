82fe6cb70c815f92dc0c96ebd79095268a15dba3
Heather Yu <heather.e.tarr310@gmail.com>
2023-04-26 22:12:17 -0400

    feat: more sets added

--- src/menuItems.js
+++ src/menuItems.js
@@ -2,32 +2,84 @@ export const menuItems = [
 
   {
     title: 'Generations',
-    url: 'generations',
+    url: 'Generations',
     logo: 'https://images.pokemontcg.io/g1/symbol.png',
   },
   {
     title: 'Flashfire',
-    url: 'flashfire',
+    url: 'Flashfire',
     logo: 'https://images.pokemontcg.io/xy2/symbol.png',
   },
   {
     title: 'XY',
-    url: 'xy',
+    url: 'XY',
     logo: 'https://images.pokemontcg.io/xy1/symbol.png',
   },
   {
     title: 'Arceus',
-    url: 'arceus',
+    url: 'Arceus',
     logo: 'https://images.pokemontcg.io/pl4/symbol.png',
   },
   {
     title: 'Platinum',
-    url: 'platinum',
+    url: 'Platinum',
     logo: 'https://images.pokemontcg.io/pl1/symbol.png',
   },
   {
     title: 'Stormfront',
-    url: 'stormfront',
+    url: 'Stormfront',
     logo: 'https://images.pokemontcg.io/dp7/symbol.png',
   },
+  {
+    title: 'Crown Zenith',
+    url: '"Crown Zenith"',
+    logo: 'https://images.pokemontcg.io/swsh12pt5/symbol.png',
+  },
+  {
+    title: 'Silver Tempest',
+    url: '"Silver Tempest"',
+    logo: 'https://images.pokemontcg.io/swsh12/symbol.png',
+  },
+  {
+    title: 'Lost Origin',
+    url: '"Lost Origin"',
+    logo: 'https://images.pokemontcg.io/swsh11/symbol.png',
+  },
+  {
+    title: 'Pokemon Go',
+    url: '"Pokemon Go"',
+    logo: 'https://images.pokemontcg.io/pgo/symbol.png',
+  },
+  {
+    title: 'Astral Radiance',
+    url: '"Astral Radiance"',
+    logo: 'https://images.pokemontcg.io/swsh10/symbol.png',
+  },
+  {
+    title: 'Brilliant Stars',
+    url: '"Brilliant Stars"',
+    logo: 'https://images.pokemontcg.io/swsh9/symbol.png',
+  },
+  {
+    title: 'Fusion Strike',
+    url: '"Fusion Strike"',
+    logo: 'https://images.pokemontcg.io/swsh8/symbol.png',
+  },
+  {
+    title: 'Celebrations',
+    url: 'Celebrations',
+    logo: 'https://images.pokemontcg.io/cel25/symbol.png',
+  },
+  {
+    title: 'Evolving Skies',
+    url: '"Evolving Skies"',
+    logo: 'https://images.pokemontcg.io/swsh7/symbol.png',
+  },
+  {
+    title: 'Chilling Reign',
+    url: '"Chilling Reign"',
+    logo: 'https://images.pokemontcg.io/swsh6/symbol.png',
+  },
+  
+  
 ]
